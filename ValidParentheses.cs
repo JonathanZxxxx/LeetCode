268baae52132a59078e9c5f40697f960763aaa7f
Zheng Xin <jonathanzhengxin@outlook.com>
2019-03-13 10:13:53 +0800

    20. Valid Parentheses
    20. 有效的括号
    栈和字典实现

---
+++ ValidParentheses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            var map = new Dictionary<char, char>
+            {
+                { '}', '{' },
+                { ']', '[' },
+                { ')', '(' }
+            };
+            var sChar = s.ToCharArray();
+            var stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                var c = sChar[i];
+                if (map.ContainsKey(c))
+                {
+                    var top = stack.Any() ? stack.Pop() : '#';
+                    if (top != map[c])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            return !stack.Any();
+        }
+    }
+}
