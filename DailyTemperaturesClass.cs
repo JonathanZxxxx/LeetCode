64963486b1115db0c8b91f6040d7ab226cca3bc9
Zheng Xin <jonathanzhengxin@outlook.com>
2019-03-14 10:36:27 +0800

    739. Daily Temperatures
    739. 每日温度
    递减栈 字典

---
+++ DailyTemperaturesClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class DailyTemperaturesClass
+    {
+        public int[] DailyTemperatures(int[] T)
+        {
+            var result = new int[T.Length];
+            var stack = new Stack<int>();
+            for (int i = 0; i < T.Length; i++)
+            {
+                while (stack.Any() && T[i] > T[stack.Peek()])
+                {
+                    result[stack.Peek()] = i - stack.Peek();
+                    stack.Pop();
+                }
+                stack.Push(i);
+            }
+            return result;
+        }
+    }
+}
