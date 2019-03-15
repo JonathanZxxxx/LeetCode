9531ca6e02b7f5b4870a942b88b2555fa2f0724d
Zheng Xin <jonathanzhengxin@outlook.com>
2019-03-15 14:49:46 +0800

    150. Evaluate Reverse Polish Notation
    150. 逆波兰表达式求值
    栈操作

---
+++ EvalRPNClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class EvalRPNClass
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            var value = new Stack<int>();
+            foreach (var s in tokens)
+            {
+                if (s != "+" && s != "-" && s != "*" && s != "/")
+                {
+                    value.Push(Convert.ToInt32(s));
+                    continue;
+                }
+                var a = value.Pop();
+                var b = value.Pop();
+                switch (s)
+                {
+                    case "+": value.Push(b + a); break;
+                    case "-": value.Push(b - a); break;
+                    case "*": value.Push(b * a); break;
+                    case "/": value.Push(b / a); break;
+                    default: break;
+                }
+            }
+            return value.Pop();
+        }
+    }
+}
