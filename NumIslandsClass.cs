796d9ad0575fd0ce4ac565c19896eb126c4938b0
Zheng Xin <jonathanzhengxin@outlook.com>
2019-03-18 16:43:22 +0800

    200. Number of Islands
    200. 岛屿的个数
    深度搜索遍历图和递归

---
+++ NumIslandsClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class NumIslandsClass
+    {
+        public int NumIslands(char[,] grid)
+        {
+            if (grid.Length == 0) return 0;
+            var result = 0;
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (grid[i, j] == '1')
+                    {
+                        result++;
+                        ToTwo(grid, i, j);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void ToTwo(char[,] grid, int i, int j)
+        {
+            if (i < 0 || j < 0 || i >= grid.GetLength(0) || j >= grid.GetLength(1) || grid[i, j] != '1')
+            {
+                return;
+            }
+            grid[i, j] = '2';
+            ToTwo(grid, i, j + 1);
+            ToTwo(grid, i, j - 1);
+            ToTwo(grid, i - 1, j);
+            ToTwo(grid, i + 1, j);
+        }
+    }
+}
