2fff7517657a0f804ec7f339e332f21a9e349cac
Zheng Xin <jonathanzhengxin@outlook.com>
2019-03-18 17:26:28 +0800

    136. Single Number
    136. 只出现一次的数字
    异或运算

---
+++ SingleNumberClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class SingleNumberClass
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var result = 0;
+            if (nums.Any())
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    result ^= nums[i];
+                }
+            }
+            return result;
+        }
+    }
+}
