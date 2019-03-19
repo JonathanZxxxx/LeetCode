7f461933a3a8518a2cd24cfab557ac756ad82af7
Zheng Xin <jonathanzhengxin@outlook.com>
2019-03-19 11:43:45 +0800

    169. Majority Element
    169. 求众数
    摩尔投票法

---
+++ MajorityElementClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class MajorityElementClass
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var count = 1;
+            var result = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (result == nums[i]) count++;
+                else
+                {
+                    count--;
+                    if (count == 0) result = nums[i + 1];
+                }
+            }
+            return result;
+        }
+    }
+}
