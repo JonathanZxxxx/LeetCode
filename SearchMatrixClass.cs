6574477a4c440a51aca5b8de318edc2544e3f6cb
Zheng Xin <jonathanzhengxin@outlook.com>
2019-03-19 17:37:08 +0800

    240. Search a 2D Matrix II
    240. 搜索二维矩阵 II
    二分查找 分治法

---
+++ SearchMatrixClass.cs
@@ -0,0 +1,45 @@
+﻿namespace Async
+{
+    public class SearchMatrixClass
+    {
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            if (matrix.Length == 0) return false;
+            //行
+            var m = matrix.GetLength(0);
+            //列
+            var n = matrix.GetLength(1);
+            for (int i = 0; i < m; i++)
+            {
+                var left = 0;
+                var right = n - 1;
+                while (left <= right)
+                {
+                    var middle = (left + right) / 2;
+                    if (matrix[i, middle] == target) return true;
+                    if (matrix[i, middle] < target) left = middle + 1;
+                    else right = middle - 1;
+                }
+            }
+            return false;
+        }
+
+        public bool SearchMatrix2(int[,] matrix, int target)
+        {
+            if (matrix.Length == 0) return false;
+            //行
+            var m = matrix.GetLength(0);
+            //列
+            var n = matrix.GetLength(1);
+            var i = 0;
+            var j = n - 1;
+            while (j >= 0 && i < m)
+            {
+                if (matrix[i, j] == target) return true;
+                if (matrix[i, j] > target) j--;
+                else i++;
+            }
+            return false;
+        }
+    }
+}
