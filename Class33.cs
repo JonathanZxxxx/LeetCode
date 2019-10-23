ec6c32dd36756826596db5bc8d27309154330d22
Zheng Xin <jonathanzhengxin@outlook.com>
2019-10-23 11:46:17 +0800

    33. 搜索旋转排序数组 Search in Rotated Sorted Array

---
+++ Class33.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class Class33
+    {
+        public int Search(int[] nums, int target)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (nums[mid] == target) return mid;
+                //左半边有序
+                if (nums[start] <= nums[mid])
+                {
+                    //target在这段里
+                    if (nums[start] <= target && target < nums[mid])
+                    {
+                        end = mid - 1;
+                    }
+                    else
+                    {
+                        start = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (nums[mid] < target && target <= nums[end])
+                    {
+                        start = mid + 1;
+                    }
+                    else
+                    {
+                        end = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
