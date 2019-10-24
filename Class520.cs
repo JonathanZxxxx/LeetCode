f609d41ec5e25077a1021f26c086c820014fb08f
Zheng Xin <jonathanzhengxin@outlook.com>
2019-10-24 15:22:29 +0800

    520. 检测大写字母 Detect Capital

---
+++ Class520.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class Class520
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            var upper = word.ToUpper();
+            var lower = word.ToLower();
+            var mix = word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower();
+            if (word == upper || word == lower || word == mix) return true;
+            return false;
+        }
+    }
+}
