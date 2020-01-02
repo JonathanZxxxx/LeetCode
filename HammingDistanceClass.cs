0d1bbb2156eeea2b7fc54880c172a349a6cde5c7
Zheng Xin <jonathanzhengxin@outlook.com>
2020-01-02 16:04:15 +0800

    461. 汉明距离 Hamming Distance

---
+++ HammingDistanceClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class HammingDistanceClass
+    {
+        public int HammingDistance(int x, int y)
+        {
+            var result = x ^ y;//二进制异或运算
+            var count = 0;
+            while (result > 0)
+            {
+                if ((result & 1) == 1)//二进制与操作
+                {
+                    count++;
+                }
+                result = result >> 1;//位右移操作
+            }
+            return count;
+        }
+    }
+}
