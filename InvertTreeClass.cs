fc64e40a563fd226f5d3a30ae627624e81ce5346
Zheng Xin <jonathanzhengxin@outlook.com>
2020-01-05 21:51:18 +0800

    226. 翻转二叉树 Invert Binary Tree

---
+++ InvertTreeClass.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class InvertTreeClass
+    {
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            var left = InvertTree(root.right);
+            var right = InvertTree(root.left);
+            root.left = left;
+            root.right = right;
+            return root;
+        }
+
+        /// <summary>
+        /// 迭代
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public TreeNode InvertTree2(TreeNode root)
+        {
+            if (root == null) return null;
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Any())
+            {
+                var pop = stack.Pop();
+                var temp = pop.left;
+                pop.left = pop.right;
+                pop.right = temp;
+                if (pop.left != null) stack.Push(pop.left);
+                if (pop.right != null) stack.Push(pop.right);
+            }
+            return root;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
