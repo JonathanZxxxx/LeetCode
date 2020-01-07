ae54baa3ce47ec9cd4efe8b43ecf8aaa030ae92b
Zheng Xin <jonathanzhengxin@outlook.com>
2020-01-07 19:43:06 +0800

    21. 合并两个有序链表 Merge Two Sorted Lists

---
+++ MergeTwoListsClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class MergeTwoListsClass
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            else if (l2 == null) return l1;
+            else if (l1.val < l2.val)
+            {
+                l1.next = MergeTwoLists(l1.next, l2);
+                return l1;
+            }
+            else
+            {
+                l2.next = MergeTwoLists(l2.next, l1);
+                return l2;
+            }
+        }
+
+        /// <summary>
+        /// 迭代
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode MergeTwoLists2(ListNode l1, ListNode l2)
+        {
+            var head = new ListNode(-1);
+            var prev = head;
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    prev.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }
+            prev.next = l1 == null ? l2 : l1;
+            return head.next;
+        }
+    }
+}
